c7cd512e7b779f3c8c10ca7372ce27b745cd40ba
Crow Rising <miscbyrd@gmail.com>
2023-05-15 16:10:40 -0600

    unit tests

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Market, type: :model do
+RSpec.describe Vendor, type: :model do
   describe 'relationships' do
     it { should have_many(:market_vendors) }
-    it { should have_many(:vendors).through(:market_vendors) }
+    it { should have_many(:markets).through(:market_vendors) }
   end
 end
